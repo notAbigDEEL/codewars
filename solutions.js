682d957951cdbd93f80356aaf20ae6cf8ca7cd20
Adeel Bukhari <adeelbukhari@gmail.com>
2021-03-14 21:47:47 -0400

    Add a codewar

--- solutions.js
+++ solutions.js
@@ -427,4 +427,29 @@ The array will always have at least 2 elements1 and all elements will be numbers
     }
 
 
-    
+    /**Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+ */
+
+    function XO(str) {
+        let countX = 0
+        let countO = 0
+       
+        for(let i=0; i<str.length;i++){
+           if(str[i].toLowerCase() === "x"){
+             countX++
+           }
+          else if(str[i].toLowerCase() === "o"){
+            countO++
+          }
+       }
+            return countX === countO
+       }
+       
