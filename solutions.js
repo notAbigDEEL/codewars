2e49ae15dc87a0c46dc9ad575e2527ee57f6f712
Adeel Bukhari <adeelbukhari@gmail.com>
2021-04-06 18:00:38 -0400

    Added kata

--- solutions.js
+++ solutions.js
@@ -1310,5 +1310,15 @@ function grader(score) {
   }
 }
 
+/*Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+ */
+
+function addBinary(a,b) {
+  const sum = a+b
+  return sum.toString(2)
+
+}
 
 
