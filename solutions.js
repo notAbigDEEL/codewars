9f055ef5abb1b57d93b7b61fdbceee95555455b5
Adeel Bukhari <adeelbukhari@gmail.com>
2021-04-24 18:00:12 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2006,5 +2006,31 @@ function caffeineBuzz(n){
   } else{
     return 'mocha_missing!'
   }
+}
+
+
+/* Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+ */
+
+
+function uefaEuro2016(teams, scores){
+  let teamA = teams[0]
+  let teamB = teams[1]
+  let scoreA = scores[0]
+  let scoreB = scores[1]
   
- }
+  if(scoreA > scoreB){
+    return `At match ${teamA} - ${teamB}, ${teamA} won!`
+  }else if(scoreB > scoreA){
+    return `At match ${teamA} - ${teamB}, ${teamB} won!`    
+  }else if(scoreA === scoreB){
+    return `At match ${teamA} - ${teamB}, teams played draw.`    
+  }
+  
+}
+
+
