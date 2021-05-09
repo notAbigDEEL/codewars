9beb6d2c34f41520ee2bab02930adac3e26af4f8
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-08 22:52:48 -0400

    Added Kata

--- solutions.js
+++ solutions.js
@@ -17,6 +17,7 @@ function adjacentElementsProduct(array) {
     };
     return p;
   }
+  
 
 /*In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
 
