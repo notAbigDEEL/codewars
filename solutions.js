b707ab2a183afac9385133e6de843f47490c6d82
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-13 20:23:18 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2580,7 +2580,7 @@ You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 3
     return (2 + 2) * ((2 + 2) * 2)
   }
 
-  
+
   /*Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
 Write a function which takes a list of strings and returns each line prepended by the correct number.
@@ -2598,4 +2598,24 @@ number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
     return array.map((el, i)=>{
       return `${i+1}: ${el}`
     })
+  }
+
+
+  /* Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+Examples
+
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+   */
+
+  var ArrowFunc = function(arr) {
+    return arr.map( x => String.fromCharCode(x) ).join(''); //Complete this function
   }
