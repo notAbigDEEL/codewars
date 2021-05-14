83c86523416677e76f51c676ee5c7de040844e02
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-13 20:00:42 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2568,3 +2568,16 @@ Return your answer as a number.
   function sumMix(x){
     return x.map(a => parseInt(a)).reduce((a,b) => a+b)
    }
+
+
+   /*Grasshopper Order of Operations
+
+You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+
+    */
+
+   function orderOperations () {
+    return (2 + 2) * ((2 + 2) * 2)
+  }
+
+  
