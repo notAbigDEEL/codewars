483366bb74dc92e4787b1101e7436baf7811ef54
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-15 22:48:14 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2541,7 +2541,6 @@ var someNumbers = [1,2,3,4,5,6,7,8,9,10];
 
 sum(someNumbers); // should return 55
 
-
  */
 
 function sum(array) {
@@ -2581,4 +2580,23 @@ function factorial(n) {
   }
 }
 
-///
+/* SpeedCode #2 - Array Madness
+Objective
+Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+E.g.
+
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+
+*/
+
+function arrayMadness(a, b) {
+  let sumOfSquares = a.map((x) => x * x).reduce((x, y) => x + y);
+  let sumOfCubes = b.map((x) => x * x * x).reduce((x, y) => x + y);
+
+  if (sumOfSquares > sumOfCubes) {
+    return true;
+  } else {
+    return false;
+  }
+}
