391f6fbcf4878e7288ea215b7de63789d20b9392
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-17 20:34:55 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2655,6 +2655,7 @@ function sumSquares(array) {
 
 /*It is easy to join two strings together like this string1 + string2.
 
+
 Another way to get the desired result would be with string1.concat(string2)
 
 ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
