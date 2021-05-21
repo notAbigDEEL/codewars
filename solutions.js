80e351d1169c074b182bd63fdda444ffc50e139a
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-20 21:44:06 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2668,3 +2668,41 @@ If one of the arguments is a number your code must coerce it into being a string
 function joinStrings(string1, string2) {
   return `${string1} ${string2}`;
 }
+
+/* Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
+
+    If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
+
+    If the sum of odd numbers is greater than the sum of even numbers return: "Odd is greater than Even"
+
+    If the total of both even and odd numbers are identical return: "Even and Odd are the same"
+
+
+ */
+
+function evenOrOdd(str) {
+
+  let strArr = str.split('')
+  let oddArr = strArr.filter((a)=> a % 2 !== 0)
+  let evenArr = strArr.filter((a)=> a % 2 === 0)
+  
+  let oddTotal = oddArr.reduce((accum, val)=>{
+    return accum + Number(val)
+  },0)
+
+  let evenTotal = evenArr.reduce((accum, val)=>{
+    return accum + Number(val)
+  },0)  
+  
+ if (evenTotal === oddTotal){
+    return 'Even and Odd are the same'
+  }
+  else if(oddTotal > evenTotal){
+    return 'Odd is greater than Even'
+  }
+  else{
+    return 'Even is greater than Odd'
+  }
+
+  
+}
