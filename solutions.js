a3dbdca7ba3617dad12cdf51d153994a275cc4d4
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-21 20:50:47 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2715,3 +2715,28 @@ This example will return the string Hello, John Smith! Welcome to Phoenix, Arizo
 function sayHello(name, city, state) {
   return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 }
+
+/* Write a function that calculates the original product price, without VAT.
+Example
+
+If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+Thus, if your function receives 230.00 as input, it should return 200.00
+
+Notes:
+
+    VAT is always 15% for the purposes of this Kata.
+    Round the result to 2 decimal places.
+    If null value given then return -1
+
+
+ */
+
+function excludingVatPrice(price) {
+  if (price === null) {
+    return -1;
+  } else {
+    let num = price / 1.15;
+    return Number(num.toFixed(2));
+  }
+}
