d61e216629ceae5110eb960fc1d6948c276ddb88
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-25 15:15:02 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3235,3 +3235,23 @@ function _if(bool, func1, func2) {
   }
 }
 
+/* Messi goals function
+
+Messi is a soccer player with goals in three leagues:
+
+    LaLiga
+    Copa del Rey
+    Champions
+
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+ */
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
