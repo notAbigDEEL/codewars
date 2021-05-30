be4c0a3e9eae0b8f611a5bb55ab70c03054678da
Adeel Bukhari <adeelbukhari@gmail.com>
2021-05-29 21:50:32 -0400

    Add kata

--- solutions.js
+++ solutions.js
@@ -3273,7 +3273,8 @@ function ifChuckSaysSo() {
 
 
 Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
- */
+
+*/
 
 function unusualFive() {
   let five = "fiver";
