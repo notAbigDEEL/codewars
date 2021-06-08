f839c610d21273f1aa7f48578396d016cc5a775a
Adeel Bukhari <adeelbukhari@gmail.com>
2021-06-08 14:19:30 -0400

    Add kata

--- solutions.js
+++ solutions.js
@@ -3280,7 +3280,8 @@ function unusualFive() {
   return five.length;
 }
 
-/* Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+/* 
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
 Examples
 
 n = 0  ==> [1]        # [2^0]
