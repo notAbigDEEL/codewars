e924a9ef053b1320368d78d33f175d9a9b51865d
Adeel Bukhari <adeelbukhari@gmail.com>
2021-06-21 19:41:34 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3373,8 +3373,7 @@ function removeSmallest(numbers) {
 }
 
 /* Implement a function that returns the minimal and the maximal value of a list (in this order).
-
-*/
+ */
 
 function getMinMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
