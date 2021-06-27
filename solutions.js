26b2f2903bd1b8a3c775b6840bcd7aef7acc6ba6
Adeel Bukhari <adeelbukhari@gmail.com>
2021-06-27 00:20:44 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3428,8 +3428,7 @@ function mergeArrays(arr1, arr2) {
 /* A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
 
 So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.
-
- */
+*/
 
 function checkTheBucket(bucket) {
   if (bucket.indexOf("gold") !== -1) {
