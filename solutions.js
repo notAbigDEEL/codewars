47da181e1ba737b2dd1b7e65f4dc2dda623a1fbc
Adeel Bukhari <adeelbukhari@gmail.com>
2021-06-29 16:13:19 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3499,3 +3499,36 @@ Note that you can receive floats as well.
 function toNumberArray(stringarray) {
   return stringarray.map(Number);
 }
+
+/*This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+Explanation:
+
+square() must return a copy of the array, containing all values squared
+cube() must return a copy of the array, containing all values cubed
+average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+sum() must return the sum of all array values
+even() must return an array of all even numbers
+odd() must return an array of all odd numbers
+Note: the original array must not be changed in any case!
+ */
+
+Array.prototype.square = function () {
+  return this.map((x) => x * x);
+};
+Array.prototype.cube = function () {
+  return this.map((x) => x * x * x);
+};
+Array.prototype.average = function () {
+  return this.reduce((a, b) => a + b, 0) / this.length;
+};
+Array.prototype.sum = function () {
+  return this.reduce((a, b) => a + b, 0);
+};
+
+Array.prototype.even = function () {
+  return this.filter((a) => a % 2 === 0);
+};
+Array.prototype.odd = function () {
+  return this.filter((a) => a % 2 === 1);
+};
