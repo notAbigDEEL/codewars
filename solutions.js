0aa9825b321ddd48583a731cd971b0c23c702d93
Adeel Bukhari <adeelbukhari@gmail.com>
2021-06-29 18:42:48 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3536,3 +3536,29 @@ Array.prototype.even = function () {
 Array.prototype.odd = function () {
   return this.filter((a) => a % 2 === 1);
 };
+
+/*Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (like the name of this kata).
+
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+Examples:
+
+spinWords("Hey fellow warriors") => "Hey wollef sroirraw" 
+spinWords("This is a test") => "This is a test" 
+spinWords("This is another test") => "This is rehtona test"
+ */
+
+function spinWords(string) {
+  let result = [];
+  let strArr = string.split(" ");
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i].length >= 5) {
+      let reverseStr = strArr[i];
+      reverseStr = reverseStr.split("").reverse().join("");
+      result.push(reverseStr);
+    } else {
+      result.push(strArr[i]);
+    }
+  }
+  return result.join(" ");
+}
