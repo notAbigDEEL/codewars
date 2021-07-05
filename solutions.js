204b4e48676649d916a9214ba03820c3223567c3
Adeel Bukhari <adeelbukhari@gmail.com>
2021-07-05 19:47:20 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3546,6 +3546,7 @@ Examples:
 spinWords("Hey fellow warriors") => "Hey wollef sroirraw" 
 spinWords("This is a test") => "This is a test" 
 spinWords("This is another test") => "This is rehtona test"
+
 */
 
 function spinWords(string) {
