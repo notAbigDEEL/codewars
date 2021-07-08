20de2d7d3672690e34d82bc7f888e6448c89efe7
Adeel Bukhari <adeelbukhari@gmail.com>
2021-07-08 19:49:44 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3591,6 +3591,7 @@ Example:
 
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
 */
 
 String.prototype.toJadenCase = function () {
