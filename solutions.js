db1ae3e62701be64b06cfc553c318af1a13dabb3
Adeel Bukhari <adeelbukhari@gmail.com>
2021-07-25 21:44:31 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3625,6 +3625,7 @@ function solve(a, b) {
 
 /*John has invited some friends. His list is:
 
+
 s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
 
 Could you make a program that
