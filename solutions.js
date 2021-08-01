9059420529007a6f5f5d80f2773024ad09d82a22
Adeel Bukhari <adeelbukhari@gmail.com>
2021-07-31 23:09:55 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3628,7 +3628,6 @@ function solve(a, b) {
 s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
 
 Could you make a program that
-
     makes this string uppercase
     gives it sorted in alphabetical order by last name.
 
