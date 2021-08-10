6e0d8305a69d5f81d6d9d040431e844959bd7f94
Adeel Bukhari <adeelbukhari@gmail.com>
2021-08-10 12:28:18 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3563,7 +3563,8 @@ function spinWords(string) {
   return result.join(" ");
 }
 
-/*In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
+/*
+In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
 
 // Remove the 3's at indices 0 and 3
 // followed by removing a 4 at index 1
