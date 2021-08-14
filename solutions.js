341b1f8429fa5762440185e7e6318e1ac08a461f
Adeel Bukhari <adeelbukhari@gmail.com>
2021-08-14 19:27:00 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3391,7 +3391,8 @@ function findShort(s) {
   return sortedSArray[0].length;
 }
 
-/* You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+/* 
+You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 
 You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 
