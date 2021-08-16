ea8eaf05deacae5eb26d7197c48cfdb3a47dd065
Adeel Bukhari <adeelbukhari@gmail.com>
2021-08-15 20:14:55 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3380,7 +3380,8 @@ function getMinMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
 }
 
-/*Simple, given a string of words, return the length of the shortest word(s).
+/*
+Simple, given a string of words, return the length of the shortest word(s).
 
 String will never be empty and you do not need to account for different data types.
 */
