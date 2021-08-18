c4b1236004f937fd9b804da67a2397a2451e7762
Adeel Bukhari <adeelbukhari@gmail.com>
2021-08-18 19:40:29 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3296,7 +3296,8 @@ function powersOfTwo(n) {
   return result;
 }
 
-/*Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
+/*
+Your task is to split the chocolate bar of given dimension n x m into small squares. Each square is of size 1x1 and unbreakable. Implement a function that will return minimum number of breaks needed.
 
 For example if you are given a chocolate bar of size 2 x 1 you can split it to single squares in just one break, but for size 3 x 1 you must do two breaks.
 
