233cb1418b2f24abf633966c76aa1744255ee3c3
Adeel Bukhari <adeelbukhari@gmail.com>
2021-08-24 15:39:41 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -3097,7 +3097,8 @@ function flattenAndSort(array) {
   return flattened.sort((a, b) => a - b);
 }
 
-/* Write function bmi that calculates body mass index (bmi = weight / height2).
+/* 
+Write function bmi that calculates body mass index (bmi = weight / height2).
 
 if bmi <= 18.5 return "Underweight"
 
