c2f9b6f716b4af5371417d6f47465a2e97c0e0f6
Adeel Bukhari <adeelbukhari@gmail.com>
2021-09-07 11:32:13 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2755,7 +2755,8 @@ function dutyFree(normPrice, discount, hol) {
   return Math.floor((hol / normPrice / discount) * 100);
 }
 
-/* Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+/* 
+Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
 
 Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
 
