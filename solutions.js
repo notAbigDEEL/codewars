f34ebda5a46f5042a065934924d374b0da6a09c6
Adeel Bukhari <adeelbukhari@gmail.com>
2021-09-08 21:25:41 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2716,7 +2716,8 @@ function sayHello(name, city, state) {
   return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`;
 }
 
-/* Write a function that calculates the original product price, without VAT.
+/* 
+Write a function that calculates the original product price, without VAT.
 Example
 
 If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
