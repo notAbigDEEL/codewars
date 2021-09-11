b25f6670364de8b3bcbe17eaa203de6dd30fd02e
Adeel Bukhari <adeelbukhari@gmail.com>
2021-09-10 21:30:22 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2669,7 +2669,8 @@ function joinStrings(string1, string2) {
   return `${string1} ${string2}`;
 }
 
-/* Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
+/* 
+Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
 
     If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
 
