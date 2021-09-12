f0d9b44d708edf7e90ea10ac583d86d965247b75
Adeel Bukhari <adeelbukhari@gmail.com>
2021-09-12 10:50:29 -0400

    Add Kata

--- solutions.js
+++ solutions.js
@@ -2621,7 +2621,8 @@ function superSize(num) {
   );
 }
 
-/* What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+/* 
+What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
 
 addLength('apple ban') => ["apple 5", "ban 3"]
 addLength('you will win') => ["you 3", "will 4", "win 3"]
